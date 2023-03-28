dec7bd2a803cb682ff80df549729a9bfeb774750
aiko38975 <125642119+aiko38975@users.noreply.github.com>
2023-03-28 18:45:22 +0900

    ビュー修正

--- app/models/spending_way.rb
+++ app/models/spending_way.rb
@@ -1,9 +1,10 @@
 class SpendingWay < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '食費・生活費' },
-    { id: 3, name: '医療費' },
-    { id: 4, name: '医療費' }
+    { id: 2, name: '食費' },
+    { id: 3, name: '生活費' },
+    { id: 4, name: '医療費' },
+    { id: 5, name: '衣類' }
   ]
 
   include ActiveHash::Associations
