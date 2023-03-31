99f8a4fef25678507e67ce40f94b0c497651908a
aiko38975 <125642119+aiko38975@users.noreply.github.com>
2023-03-31 17:43:58 +0900

    統計ページ(show)の作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to:  "tops#index"
   resources :tops, only: [:index]
-  resources :records, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :records, only: [:index, :new, :create, :edit, :update, :destroy, :show]
 
 end
