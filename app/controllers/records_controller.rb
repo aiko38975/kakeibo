7417381cb680650b36bb4ba50dda7909c53bf747
aiko38975 <125642119+aiko38975@users.noreply.github.com>
2023-04-06 11:58:55 +0900

    不要記述の削除

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -4,13 +4,7 @@ class RecordsController < ApplicationController
 
   def index
     @record = Record.new
-    # year = Date.today.year
-    # month = Date.today.month
-    # year = params[:recorded_at][:year].to_i
-    # month = params[:recorded_at][:month].to_i
-    # date = Date.new(year, month)
     @records = Record.includes(:user).order("recorded_at DESC")
-    # @records = Record.where(recorded_at: date.beginning_of_month..date.end_of_month)
   end
   
   def new
@@ -60,9 +54,5 @@ class RecordsController < ApplicationController
   def record_params
     params.require(:record).permit(:recorded_at, :spending_way_id, :spending_pay_id, :price, :description).merge(user_id: current_user.id)
   end
-
-  # def search_params
-  #   params.fetch(:search, {}).permit(:recorded_at)
-  # end
 
 end
