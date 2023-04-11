d27c420492b32b6fd66a6f41b0d0476416a17081
aiko38975 <125642119+aiko38975@users.noreply.github.com>
2023-04-11 15:14:28 +0900

    修正

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,8 +1,9 @@
 class Record < ApplicationRecord
-  validates :recorded_at, presence: true
-  validates :spending_way_id, presence: true, numericality: { other_than: 1, message: "を選択してください。" }
-  validates :spending_pay_id, presence: true, numericality: { other_than: 1, message: "を選択してください。" }
-  validates :price, presence: true,numericality: { only_integer: true }
+  # validates :recorded_at, presence: true
+  # validates :spending_way_id, presence: true, numericality: { other_than: 1, message: "を選択してください。" }
+  # validates :spending_pay_id, presence: true, numericality: { other_than: 1, message: "を選択してください。" }
+  # validates :price, presence: true,numericality: { only_integer: true }
+
 
   belongs_to :user
  
